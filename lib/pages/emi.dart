f11e66a746dc9507f70eabdd9d2204d9d9464cd7
mercurystudio99 <mercurystudio99@gmail.com>
2023-07-27 07:34:12 -0700

    update emi function

--- lib/pages/emi.dart
+++ lib/pages/emi.dart
@@ -10,11 +10,42 @@ class EMIPage extends StatefulWidget {
 }
 
 class _EMIPageState extends State<EMIPage> {
-  final List<ChartData> chartData = [
-    ChartData('principal', 25, Colors.blue.shade900),
-    ChartData('tax', 38, Colors.blue.shade600),
-    ChartData('insurance', 52, Colors.blue.shade200)
+  late double _result = 0;
+  late double _rate = 0;
+  late double _tax = 0;
+  late double _insurance = 0;
+  late List<ChartData> chartData = [
+    ChartData('principal', 0, Colors.blue.shade900),
+    ChartData('tax', 0, Colors.blue.shade600),
+    ChartData('insurance', 0, Colors.blue.shade200)
   ];
+
+  final _totalamountController = TextEditingController();
+  final _downpaymentController = TextEditingController();
+  final _interestrateController = TextEditingController();
+  final _loantermsController = TextEditingController();
+  final _taxController = TextEditingController();
+  final _insuranceController = TextEditingController();
+
+  void _calculate() {
+    double total = double.parse(_totalamountController.text.trim());
+    double down = double.parse(_downpaymentController.text.trim());
+    double rate = double.parse(_interestrateController.text.trim());
+    double terms = double.parse(_loantermsController.text.trim());
+    double tax = double.parse(_taxController.text.trim());
+    double insurance = double.parse(_insuranceController.text.trim());
+    _rate = rate;
+    _tax = tax;
+    _insurance = insurance;
+    _result = total + down + rate * terms + tax * insurance;
+    chartData = [
+      ChartData('principal', _rate, Colors.blue.shade900),
+      ChartData('tax', _tax, Colors.blue.shade600),
+      ChartData('insurance', _insurance, Colors.blue.shade200)
+    ];
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,8 +82,10 @@ class _EMIPageState extends State<EMIPage> {
           child: Row(children: [
             Expanded(
               child: TextField(
+                controller: _totalamountController,
+                keyboardType: TextInputType.number,
                 decoration: InputDecoration(
-                  labelText: 'Total Amount(\$)',
+                  labelText: 'Total Amount(\₹)',
                   border: UnderlineInputBorder(),
                 ),
               ),
@@ -60,6 +93,8 @@ class _EMIPageState extends State<EMIPage> {
             const SizedBox(width: 25),
             Expanded(
               child: TextField(
+                controller: _downpaymentController,
+                keyboardType: TextInputType.number,
                 decoration: InputDecoration(
                   labelText: 'Down Payment',
                   border: UnderlineInputBorder(),
@@ -72,6 +107,8 @@ class _EMIPageState extends State<EMIPage> {
           child: Row(children: [
             Expanded(
               child: TextField(
+                controller: _interestrateController,
+                keyboardType: TextInputType.number,
                 decoration: InputDecoration(
                   labelText: 'Interest Rate(%)',
                   border: UnderlineInputBorder(),
@@ -81,6 +118,8 @@ class _EMIPageState extends State<EMIPage> {
             const SizedBox(width: 25),
             Expanded(
               child: TextField(
+                controller: _loantermsController,
+                keyboardType: TextInputType.number,
                 decoration: InputDecoration(
                   labelText: 'Loan Terms(Years)',
                   border: UnderlineInputBorder(),
@@ -93,8 +132,10 @@ class _EMIPageState extends State<EMIPage> {
           child: Row(children: [
             Expanded(
               child: TextField(
+                controller: _taxController,
+                keyboardType: TextInputType.number,
                 decoration: InputDecoration(
-                  labelText: 'Property Tax(\$)',
+                  labelText: 'Property Tax(\₹)',
                   border: UnderlineInputBorder(),
                 ),
               ),
@@ -102,8 +143,10 @@ class _EMIPageState extends State<EMIPage> {
             const SizedBox(width: 25),
             Expanded(
               child: TextField(
+                controller: _insuranceController,
+                keyboardType: TextInputType.number,
                 decoration: InputDecoration(
-                  labelText: 'Home Insurance(\$)',
+                  labelText: 'Home Insurance(\₹)',
                   border: UnderlineInputBorder(),
                 ),
               ),
@@ -126,7 +169,7 @@ class _EMIPageState extends State<EMIPage> {
                   padding:
                       const EdgeInsets.all(5) //content padding inside button
                   ),
-              onPressed: () {},
+              onPressed: _calculate,
               child: const Text(
                 'CALCULATE',
                 style: TextStyle(
@@ -145,7 +188,7 @@ class _EMIPageState extends State<EMIPage> {
             textAlign: TextAlign.center,
             text: TextSpan(children: [
               TextSpan(
-                  text: '\$752',
+                  text: '\₹$_result',
                   style: TextStyle(
                       color: Colors.black,
                       fontWeight: FontWeight.bold,
@@ -185,7 +228,7 @@ class _EMIPageState extends State<EMIPage> {
             const SizedBox(width: 10),
             const Text('Principal & Interest'),
             const Spacer(),
-            const Text('419')
+            Text(_rate.toString())
           ])),
       Padding(
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
@@ -201,7 +244,7 @@ class _EMIPageState extends State<EMIPage> {
             const SizedBox(width: 10),
             const Text('Property Tax'),
             const Spacer(),
-            const Text('250')
+            Text(_tax.toString())
           ])),
       Padding(
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
@@ -217,7 +260,7 @@ class _EMIPageState extends State<EMIPage> {
             const SizedBox(width: 10),
             const Text('Home Insurance'),
             const Spacer(),
-            const Text('83')
+            Text(_insurance.toString())
           ])),
       const SizedBox(height: 80)
     ])));
